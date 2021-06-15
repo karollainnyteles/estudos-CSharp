6388746f798a84ac1a08271eab162322ebdfde68
Karollainny <karollainny.teles@mail.uft.edu.br>
2021-06-15 10:32:23 -0300

    Entendo Exceções

---
+++ ContaCorrente.cs
@@ -0,0 +1,112 @@
+﻿//using _05_ByteBank;
+
+using _05_ByteBank;
+using System;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public int contadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+        public double TaxaOperacao { get; private set; }
+        public static int TotalContasCriadas { get; private set; }
+
+        //cria uma propriedade com getter e setter
+        public Cliente Titular { get; set; }
+        public int Agencia { get; }
+        public int Numero { get; }
+
+        private double _saldo = 100;
+
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if(agencia <=0)
+            {
+              throw new ArgumentException("A agencia deve ser maior que 0",nameof(agencia));
+                
+            }
+
+            if (numero <= 0)
+            {
+               throw new ArgumentException("O numero deve ser maior que 0",nameof(numero));
+              
+            }
+
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalContasCriadas++;
+
+            TaxaOperacao = 30 / TotalContasCriadas;
+        }
+
+
+
+        public void  Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para o saque", nameof(valor));
+            }
+            if (_saldo < valor)
+            {
+                contadorSaquesNaoPermitidos++;
+
+                throw new SaldoInsuficienteException(Saldo,valor);
+               
+            }
+
+            _saldo -= valor;
+            
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para a transferência", nameof(valor));
+            }
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException e)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw new OperacaoFinanceiraException("Operação não realizada.",e);
+            }
+
+
+            
+            contaDestino.Depositar(valor);
+           
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using _05_ByteBank;
+using System;
+using System.IO;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                CarregarContas();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Catch na main");
+            }
+           
+
+
+        }
+        private static void CarregarContas()
+        {
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor= new LeitorDeArquivo("contas.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+
+                
+            //}
+            //catch (IOException)
+            //{
+               
+            //    Console.WriteLine("Exceção do tipo IOException");
+            //}
+            //finally
+            //{
+            //    if (leitor != null)
+            //    {
+
+            //        leitor.Fechar();
+            //    }
+            //}
+
+
+
+
+        }
+        private static void TestaExceptions()
+        {
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente(41, 14222);
+                ContaCorrente conta2 = new ContaCorrente(741, 47585);
+                //conta1.Transferir(150, conta2);
+                conta1.Sacar(300);
+            }
+
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Problema com o argumento: " + e.ParamName);
+                Console.WriteLine("Ocorreu um erro de argumento");
+                Console.WriteLine(e.Message);
+            }
+
+            catch (OperacaoFinanceiraException ex)
+
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("********************");
+                Console.WriteLine(ex.InnerException.Message);
+                Console.WriteLine(ex.InnerException.StackTrace);
+
+
+            }
+        }
+    }
+}
